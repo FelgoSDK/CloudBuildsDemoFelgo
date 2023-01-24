05581779f998a7e9c963aad352f598cc1a062aba
BuildAutomation <ba@v-play.net>
2023-01-24 18:36:05 +0100

    Update with Qt 6, Felgo 4, CMake support (for Qt 6/Felgo 3, use the qt5 branch)

--- main.cpp
+++ main.cpp
@@ -9,35 +9,35 @@
 int main(int argc, char *argv[])
 {
 
-    QApplication app(argc, argv);
+  QApplication app(argc, argv);
 
-    FelgoApplication felgo;
+  FelgoApplication felgo;
 
-    // Use platform-specific fonts instead of Felgo's default font
-    felgo.setPreservePlatformFonts(true);
+  // Use platform-specific fonts instead of Felgo's default font
+  felgo.setPreservePlatformFonts(true);
 
-    QQmlApplicationEngine engine;
-    felgo.initialize(&engine);
+  QQmlApplicationEngine engine;
+  felgo.initialize(&engine);
 
-    // Set an optional license key from project file
-    // This does not work if using Felgo Live, only for Felgo Cloud Builds and local builds
-    felgo.setLicenseKey(PRODUCT_LICENSE_KEY);
+  // Set an optional license key from project file
+  // This does not work if using Felgo Live, only for Felgo Cloud Builds and local builds
+  felgo.setLicenseKey(PRODUCT_LICENSE_KEY);
 
-    // use this during development
-    // for PUBLISHING, use the entry point below
-    felgo.setMainQmlFileName(QStringLiteral("qml/Main.qml"));
+  // use this during development
+  // for PUBLISHING, use the entry point below
+  felgo.setMainQmlFileName(QStringLiteral("qml/Main.qml"));
 
-    // use this instead of the above call to avoid deployment of the qml files and compile them into the binary with qt's resource system qrc
-    // this is the preferred deployment option for publishing games to the app stores, because then your qml files and js files are protected
-    // to avoid deployment of your qml files and images, also comment the DEPLOYMENTFOLDERS command in the .pro file
-    // also see the .pro file for more details
-    felgo.setMainQmlFileName(QStringLiteral("qrc:/qml/Main.qml"));
+  // use this instead of the above call to avoid deployment of the qml files and compile them into the binary with qt's resource system qrc
+  // this is the preferred deployment option for publishing games to the app stores, because then your qml files and js files are protected
+  // to avoid deployment of your qml files and images, also comment the DEPLOYMENTFOLDERS command in the .pro file
+  // also see the .pro file for more details
+  //felgo.setMainQmlFileName(QStringLiteral("qrc:/qml/Main.qml"));
 
-    engine.load(QUrl(felgo.mainQmlFileName()));
+  engine.load(QUrl(felgo.mainQmlFileName()));
 
-    // to start your project as Live Client, comment (remove) the lines "felgo.setMainQmlFileName ..." & "engine.load ...",
-    // and uncomment the line below
-    //FelgoLiveClient client (&engine);
+  // to start your project as Live Client, comment (remove) the lines "felgo.setMainQmlFileName ..." & "engine.load ...",
+  // and uncomment the line below
+  //FelgoLiveClient client (&engine);
 
-    return app.exec();
+  return app.exec();
 }
