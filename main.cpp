fb1e194dcbbec1421f00cc81f675184216a3dbc8
BuildAutomation <ba@v-play.net>
2024-08-19 18:07:36 +0200

    Update project to support/target Android 14 (API 34)

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 #include <QQmlApplicationEngine>
 
-// uncomment this line to add the Live Client Module and use live reloading with your custom C++ code
-//#include <FelgoLiveClient>
+// Uncomment this line to add Felgo Hot Reload and use hot reloading with your custom C++ code
+//#include <FelgoHotReload>
 
 int main(int argc, char *argv[])
 {
@@ -13,14 +13,11 @@ int main(int argc, char *argv[])
 
   FelgoApplication felgo;
 
-  // Use platform-specific fonts instead of Felgo's default font
-  felgo.setPreservePlatformFonts(true);
-
   QQmlApplicationEngine engine;
   felgo.initialize(&engine);
 
   // Set an optional license key from project file
-  // This does not work if using Felgo Live, only for Felgo Cloud Builds and local builds
+  // This does not work if using Felgo Developer App, only for Felgo Cloud Builds and local builds
   felgo.setLicenseKey(PRODUCT_LICENSE_KEY);
 
   // use this during development
@@ -28,16 +25,16 @@ int main(int argc, char *argv[])
   felgo.setMainQmlFileName(QStringLiteral("qml/Main.qml"));
 
   // use this instead of the above call to avoid deployment of the qml files and compile them into the binary with qt's resource system qrc
-  // this is the preferred deployment option for publishing games to the app stores, because then your qml files and js files are protected
-  // to avoid deployment of your qml files and images, also comment the DEPLOYMENTFOLDERS command in the .pro file
-  // also see the .pro file for more details
-  //felgo.setMainQmlFileName(QStringLiteral("qrc:/qml/Main.qml"));
+  // this is the preferred deployment option for publishing apps to the app stores, because then your qml files and js files are protected
+  // to avoid deployment of your qml files and images, also comment the deploy_resources command in the CMakeLists file
+  // also see the CMakeLists.txt file for more details
+  // felgo.setMainQmlFileName(QStringLiteral("qrc:/qml/Main.qml"));
 
   engine.load(QUrl(felgo.mainQmlFileName()));
 
-  // to start your project as Live Client, comment (remove) the lines "felgo.setMainQmlFileName ..." & "engine.load ...",
+  // to start your project with Felgo Hot Reload, comment (remove) the lines "felgo.setMainQmlFileName ..." & "engine.load ...",
   // and uncomment the line below
-  //FelgoLiveClient client (&engine);
+  //FelgoHotReload felgoHotReload(&engine);
 
   return app.exec();
 }
